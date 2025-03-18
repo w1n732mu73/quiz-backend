ba78fc9c0aac4384d5097b31b5522c105c6227b5
w1n732mu73 <box16112021@gmail.com>
2025-03-18 12:26:39 +0300

    Remove excessive fields from quizes index response

--- controllers/QuizesController.js
+++ controllers/QuizesController.js
@@ -2,7 +2,7 @@ const Quiz = require("../models/quiz.js");
 
 module.exports = {
   index: async (req, res) => {
-    const quizes = await Quiz.find({})
+    const quizes = await Quiz.find({}, { _id: 1, name: 1 })
     res.json(quizes)
   }
 }
